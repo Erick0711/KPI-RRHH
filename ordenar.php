313b978a908f24484c6fa68f9e07fe90b078d0e6
maicolarteaga <maicolarteaga0711@gmail.com>
2022-12-31 11:58:52 -0400

    SE AGREGO MAS CAMBIOS PARA KPI A SOLUCIONAR

--- ordenar.php
+++ ordenar.php
@@ -19,20 +19,30 @@ function agregarConcepto($descripcion)
         }
     }
 }
+
 function agregarKpi($concepto,$periodo,$valor)
 {
     // echo $concepto,$periodo,$valor;
     include("./conexion.php");
-    $sentencia = $conexion->prepare("INSERT INTO rrhh_datos (id,
-                                    concepto,
-                                    periodo,
-                                    valor) 
-                                    VALUES(NULL, 
-                                    '$concepto',
-                                    '$periodo',
-                                    '$valor')");
-    $sentencia->execute();
+    $buscadorConcepto = $conexion->prepare("SELECT * FROM prueba WHERE descripcion='$concepto'");
+    $buscadorConcepto->execute();
+    $registros = $buscadorConcepto->fetchAll(PDO::FETCH_ASSOC);
+    if(isset($registros)){
+        $idConcepto = $registros[0]['id'];
+        $sentencia = $conexion->prepare("INSERT INTO rrhh_datos (id,
+        concepto_id,
+        periodo,
+        valor) 
+        VALUES(NULL, 
+        '$idConcepto',
+        '$periodo',
+        '$valor')");
+        $sentencia->execute();
+    }else{
+        echo "No existe el concepto";
+    }
 }
+$fecha = ['202101',	'202102', '202103',	'202104', '202105', '202106', '202107',	'202108', '202109',	'202110', '202111','202112', '202201', '202202', '202203',	'202204', '202205',	'202206', '202207', '202208', '202209',	'202210','202211','202212'];
 function convertirArreglo()
 {
     // OBTENER EL DATO DEL POST Y CONVERTIRLO A UN ARRAY
@@ -53,23 +63,26 @@ function convertirArreglo()
 
 function obtener()
 {
-    // RETURNA TODO EL ARRAY CONVERTIDO DEL LA FUNCION
+    // RETURNA TODO EL ARRAY CONVERTIDO DE LA FUNCION
     $convertir = convertirArreglo();
-    $fecha = $convertir[0]; # ME TRAE DE MANERA MANUAL TODO LAS FECHAS QUE ESTAN EN LA POSICION 0
-    $valor = $convertir[1];
+    $fecha = $convertir[0]; # ME TRAE DE MANERA MANUAL TODAS LAS FECHAS QUE ESTAN EN LA POSICION 0
+    $valor = $convertir[1]; # ME TRAE DE MANERA MANUAL TODOS LOS VALORES QUE ESTAN EN LA POSICION 1
 
-    // for ($j=1; $j < count($valor) ; $j++) { 
-    //     $valores = $valor[$j];
-    //     print_r($valores);
-    // }
-    for ($i = 0; $i < 2; $i++) {
+    for($i = 1; $i < count($valor); $i++ ){
+        print_r($convertir[$i]);
+    }
+
+    for ($i = 0; $i < 2; $i++) 
+    {
         unset($fecha[$i]);  # RECORRO PARA ELIMINAR LOS CAMPOS VACIOS O NULOS
         unset($valor[$i]);
     }
-    // for ($j=2; $j < count($valor)  ; $j++) { 
-    //     $valores = $valor[$j];
-    //     echo ($valores)."<br>";
-    // }
+    // $valores = array_values($valor); 
+    // print_r($valores);
+    die();
+
+    $fechas = array_values($fecha);
+    // print_r($valores);
     $conceptoArreglo = array();
     $datoConcepto = "";
 
@@ -77,13 +90,11 @@ function obtener()
         $concepto = $convertir[$i][1];
         $datoConcepto = $concepto;
         agregarConcepto($datoConcepto);
-        // if(empty($datoConcepto)){
-        //     break;
-        // }
+
         array_push($conceptoArreglo, $concepto); # CONVIERTO LO ENCONTRADO A UN ARREGLO Y LO CARGO AL A VARIABLE CONCEPTOARREGLO
     }
 
-    $arreglo = array($fecha, $valor, $conceptoArreglo);
+    $arreglo = array($fechas, $valores, $conceptoArreglo);
     return $arreglo;
 }
 
@@ -119,14 +130,14 @@ $arreglo = obtener();
                         <?php
                         list($fechas, $valores, $conceptos) = $arreglo;
                         ?>
-                        <?php for ($i = 2; $i < count($fechas); $i++) {  ?>
+                        <?php for ($i = 0; $i < count($fechas); $i++) {  ?>
                             <tr>
                                 <td><?php echo $conceptos[1] ?></td>
                                 <td><?php echo $fechas[$i] ?></td>
                                 <td><?php echo $valores[$i] ?></td>
                             </tr>
                         <?php 
-                        agregarKpi($conceptos[1],$fechas[$i],$valores[$i]);
+                        // agregarKpi($conceptos[1],$fechas[$i],$valores[$i]);
                     } ?>
                     </tbody>
                 </table>
